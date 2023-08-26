c6f953404f1133ee504b37ec4af04e8bd99bb87b
Tiago Spadetto dos Santos <tianospadetto@gmail.com>
2023-08-25 22:33:56 -0300

    Commit Inicial

---
+++ src/main/java/br/com/eduplan/EduplanApplication.java
@@ -0,0 +1,13 @@
+package br.com.eduplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduplanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduplanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/eduplan/EduplanApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.eduplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduplanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
